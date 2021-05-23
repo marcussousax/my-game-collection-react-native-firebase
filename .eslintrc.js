7cde542c4eb3fffb22f5a1b182806c0d3d965a1c
Marcus Sousa <marvinsousa83@gmail.com>
2021-05-23 14:18:26 -0300

    enable inline styles (react-native/no-inline-styles)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     extends: '@react-native-community',
     rules: {
         'comma-dangle': ['error', 'never'],
-        semi: ['error', 'never']
+        semi: ['error', 'never'],
+        'react-native/no-inline-styles': 0
     }
 }
