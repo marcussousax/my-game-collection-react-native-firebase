86ee52e59835ed6ed2ba7bfe882715f3424d78c8
Marcus Sousa <marvinsousa83@gmail.com>
2021-05-24 08:11:38 -0300

    Login Screen vertical and horizontal Button align

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { GoogleSigninButton } from '@react-native-google-signin/google-signin'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { AuthContext } from '../contexts/auth'
 
 const LoginScreen: React.FC = () => {
     const { signIn } = React.useContext(AuthContext)
 
     return (
-        <View>
+        <View style={styles.container}>
             <GoogleSigninButton
                 style={{ width: 192, height: 48 }}
                 size={GoogleSigninButton.Size.Wide}
@@ -17,5 +17,13 @@ const LoginScreen: React.FC = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default LoginScreen
